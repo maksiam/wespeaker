dc2c946c4a9afea1b48a333d9efdecfb89d92fab
Maksim Iogan <54811685+maksiam@users.noreply.github.com>
2024-04-03 15:19:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = [
     "kaldiio",
     "torch>=1.12.0",
     "torchaudio>=0.12.0",
-    "silero-vad @ git+https://github.com/pengzhendong/silero-vad.git",
+    "silero-vad @ git+https://github.com/pengzhendong/silero-vad.git@b9a8ecbd293dbf46697fcb3cec54c7235e14b6c2",
 ]
 
 setup(
